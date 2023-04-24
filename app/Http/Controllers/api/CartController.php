7ddc2834d58a48087781b07fcb2bf439d20491d2
hugomassaria <hugo.massaria@epitech.eu>
2023-04-24 17:45:58 +0200

    Cart and Orders Updated

--- app/Http/Controllers/api/CartController.php
+++ app/Http/Controllers/api/CartController.php
@@ -19,16 +19,23 @@ class CartController extends Controller {
             if (empty($request->user()->session_id)) {
                 return response()->json(["message" => "Empty cart."]);
             } else {
-                $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
-                foreach ($items as &$item) {
-                    $price = $stripe->prices->retrieve($item->price->id, []);
-                    $product = $stripe->products->retrieve($price->product, []);
-                    $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => $item->quantity]]);
+                $session = $stripe->checkout->sessions->retrieve($request->user()->session_id);
+                if ($session->payment_status !== "paid") {
+                    $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
+                    foreach ($items as &$item) {
+                        $price = $stripe->prices->retrieve($item->price->id, []);
+                        $product = $stripe->products->retrieve($price->product, []);
+                        $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => $item->quantity]]);
+                    }
+                } else {
+                    $request->user()->session_id = null;
+                    $request->user()->save();
+                    return response()->json(["message" => "Empty cart."]);
                 }
             }
             return response()->json($products);
         } catch (Throwable $e) {
-            return response()->json(["message" => "Internal server error."])->setStatusCode(500);
+            return response()->json(["error" => "Internal server error."])->setStatusCode(500);
         }
     }
 
@@ -43,20 +50,30 @@ class CartController extends Controller {
                 $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => 1]]);
                 $prices = array_merge($prices, [$product->default_price => 1]);
             } else {
-                $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
-                foreach ($items as &$item) {
-                    $prices = array_merge($prices, [$item->price->id => $item->quantity]);
-                    $price = $stripe->prices->retrieve($item->price->id, []);
-                    $product = $stripe->products->retrieve($price->product, []);
-                    $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => $id === $product->id ? $item->quantity + 1 : $item->quantity]]);
-                }
-                $product = $stripe->products->retrieve($id);
-                $price = $stripe->prices->retrieve($product->default_price);
-                if (array_key_exists($product->default_price, $prices)) {
-                    $prices = array_merge($prices, [$product->default_price => $prices[$product->default_price] + 1]);
+                $session = $stripe->checkout->sessions->retrieve($request->user()->session_id);
+                if ($session->payment_status !== "paid") {
+                    $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
+                    foreach ($items as &$item) {
+                        $prices = array_merge($prices, [$item->price->id => $item->quantity]);
+                        $price = $stripe->prices->retrieve($item->price->id, []);
+                        $product = $stripe->products->retrieve($price->product, []);
+                        $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => $id === $product->id ? $item->quantity + 1 : $item->quantity]]);
+                    }
+                    $product = $stripe->products->retrieve($id);
+                    $price = $stripe->prices->retrieve($product->default_price);
+                    if (array_key_exists($product->default_price, $prices)) {
+                        $prices = array_merge($prices, [$product->default_price => $prices[$product->default_price] + 1]);
+                    } else {
+                        $prices = array_merge($prices, [$product->default_price => 1]);
+                        $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => 1]]);
+                    }
                 } else {
-                    $prices = array_merge($prices, [$product->default_price => 1]);
+                    $request->user()->session_id = null;
+                    $request->user()->save();
+                    $product = $stripe->products->retrieve($id);
+                    $price = $stripe->prices->retrieve($product->default_price);
                     $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => 1]]);
+                    $prices = array_merge($prices, [$product->default_price => 1]);
                 }
             }
             $checkout = $request->user()->checkout($prices, [
@@ -70,7 +87,7 @@ class CartController extends Controller {
             $object = (object) ["id" => $checkout->id, "totalPrice" => $checkout->amount_total, "creationDate" => date("Y-m-d H:m:s", $checkout->created), "products" => $products, "url" => $checkout->url];
             return response()->json($object);
         } catch (Throwable $e) {
-            return response()->json(["message" => "Internal server error."])->setStatusCode(500);
+            return response()->json(["error" => "Internal server error."])->setStatusCode(500);
         }
     }
 
@@ -82,23 +99,30 @@ class CartController extends Controller {
             if (empty($request->user()->session_id)) {
                 return response()->json(["message" => "Can not remove product from an empty cart."])->setStatusCode(400);
             } else {
-                $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
-                foreach ($items as &$item) {
-                    $prices = array_merge($prices, [$item->price->id => $item->quantity]);
-                    $price = $stripe->prices->retrieve($item->price->id, []);
-                    $product = $stripe->products->retrieve($price->product, []);
-                    $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => $id === $product->id ? $item->quantity - 1 : $item->quantity]]);
-                }
-                $product = $stripe->products->retrieve($id);
-                $price = $stripe->prices->retrieve($product->default_price);
-                if (array_key_exists($product->default_price, $prices)) {
-                    if ($prices[$product->default_price] !== 1) {
-                        $prices = array_merge($prices, [$product->default_price => $prices[$product->default_price] - 1]);
+                $session = $stripe->checkout->sessions->retrieve($request->user()->session_id);
+                if ($session->payment_status !== "paid") {
+                    $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
+                    foreach ($items as &$item) {
+                        $prices = array_merge($prices, [$item->price->id => $item->quantity]);
+                        $price = $stripe->prices->retrieve($item->price->id, []);
+                        $product = $stripe->products->retrieve($price->product, []);
+                        $products = array_merge($products, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $price->unit_amount / 100, "quantity" => $id === $product->id ? $item->quantity - 1 : $item->quantity]]);
+                    }
+                    $product = $stripe->products->retrieve($id);
+                    $price = $stripe->prices->retrieve($product->default_price);
+                    if (array_key_exists($product->default_price, $prices)) {
+                        if ($prices[$product->default_price] !== 1) {
+                            $prices = array_merge($prices, [$product->default_price => $prices[$product->default_price] - 1]);
+                        } else {
+                            unset($prices[$product->default_price]);
+                        }
                     } else {
-                        unset($prices[$product->default_price]);
+                        return response()->json(["message" => "No product found."])->setStatusCode(400);
                     }
                 } else {
-                    return response()->json(["message" => "No product found."])->setStatusCode(400);
+                    $request->user()->session_id = null;
+                    $request->user()->save();
+                    return response()->json(["message" => "Can not remove product from an empty cart."])->setStatusCode(400);
                 }
             }
             $object = [];
@@ -119,7 +143,7 @@ class CartController extends Controller {
             }
             return response()->json($object);
         } catch (Throwable $e) {
-            return response()->json(["message" => "Internal server error."])->setStatusCode(500);
+            return response()->json(["error" => "Internal server error."])->setStatusCode(500);
         }
     }
 
@@ -129,25 +153,32 @@ class CartController extends Controller {
             if (empty($request->user()->session_id)) {
                 return response()->json(["message" => "Can not validate an empty cart."])->setStatusCode(400);
             } else {
-                $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
-                $prices = [];
-                foreach ($items as &$item) {
-                    $prices = array_merge($prices, [$item->price->id => $item->quantity]);
+                $session = $stripe->checkout->sessions->retrieve($request->user()->session_id);
+                if ($session->payment_status !== "paid") {
+                    $items = $stripe->checkout->sessions->allLineItems($request->user()->session_id);
+                    $prices = [];
+                    foreach ($items as &$item) {
+                        $prices = array_merge($prices, [$item->price->id => $item->quantity]);
+                    }
+                    $checkout = $request->user()->checkout($prices, [
+                        "mode" => "payment",
+                        "invoice_creation" => ["enabled" => true],
+                        "success_url" => route("success"),
+                        "cancel_url" => route("success"),
+                        "client_reference_id" => $request->user()->stripe_id,
+                        "customer" => $request->user()->stripe_id
+                    ]);
+                    $request->user()->session_id = $checkout->id;
+                    $request->user()->save();
+                    return response()->json(["url" => $checkout->url]);
+                } else {
+                    $request->user()->session_id = null;
+                    $request->user()->save();
+                    return response()->json(["message" => "Empty cart."]);
                 }
-                $checkout = $request->user()->checkout($prices, [
-                    "mode" => "payment",
-                    "invoice_creation" => ["enabled" => true],
-                    "success_url" => route("success"),
-                    "cancel_url" => route("success"),
-                    "client_reference_id" => $request->user()->stripe_id,
-                    "customer" => $request->user()->stripe_id
-                ]);
-                $request->user()->session_id = $checkout->id;
-                $request->user()->save();
-                return response()->json(["url" => $checkout->url]);
             }
         } catch (Throwable $e) {
-            return response()->json(["message" => "Internal server error."])->setStatusCode(500);
+            return response()->json(["error" => "Internal server error."])->setStatusCode(500);
         }
     }
 }
--- app/Http/Controllers/api/OrdersController.php
+++ app/Http/Controllers/api/OrdersController.php
@@ -11,11 +11,27 @@ class OrdersController extends Controller {
     public function index(Request $request) {
         try {
             $stripe = new \Stripe\StripeClient(env("STRIPE_SECRET"));
-            $invoices = $stripe->invoices->search(["query" => 'customer:"'.$request->user()->stripe_id.'"']);
-            
-            return response()->json($invoices);
+            $response = (array) $stripe->invoices->search(["query" => 'customer:"'.$request->user()->stripe_id.'"'])["data"];
+            if (empty($response)) {
+                return response()->json(["message" => "Empty previous orders."]);
+            } else {
+                $invoices = array_reduce($response, function($accumulator, $item) {
+                    if ($item->paid === true) {
+                        $products = array_reduce((array) $item->lines->data, function($accumulator, $item) {
+                            $stripe = new \Stripe\StripeClient(env("STRIPE_SECRET"));
+                            $product = $stripe->products->retrieve($item->price->product);
+                            array_push($accumulator, [(object) ["id" => $product->id, "name" => $product->name, "description" => $product->description, "photo" => (!$product->images) ? "" : $product->images[0], "price" => $item->price->unit_amount / 100, "quantity" => $item->quantity]]);
+                            return $accumulator;
+                        }, []);
+                        array_push($accumulator, (object) ["id" => $item->id, "totalPrice" => $item->amount_paid, "creationDate" => date("Y-m-d H:m:s", $item->created), "products" => $products, "url" => $item->hosted_invoice_url]);
+                    }
+                    return $accumulator;
+                }, []);
+                return response()->json($invoices);
+            }
+            return response()->json($response);
         } catch (Throwable $e) {
-            return response()->json(["message" => "Internal server error."])->setStatusCode(500);
+            return response()->json(["error" => "Internal server error."])->setStatusCode(500);
         }
     }
 }
